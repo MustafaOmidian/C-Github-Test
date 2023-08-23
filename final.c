bde23d32658e887218925dbc0c7ad7a3d0a2a073
mustafa omidian <mustafaomidian@gmail.com>
2023-08-23 16:47:33 +0330

    Second COMMIT

--- final.c
+++ final.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <pthread.h>
 
-
-void *myThreadFun(void *vargp)
+void *print_numbers(void *arg)
 {
-	//sleep(1);
-	printf("Printing Something");
+	int *num = (int *)arg;
+	for(int i = 0; i < 5; i++ )
+	{
+		printf("Thread %d: %d\n", *num,i);
+	}
 	return NULL;
 }
-
 int main()
 {
-	pthread_t thread_id;
-	printf("before thread\n");
-	pthread_create(&thread_id, NULL,myThreadFun, NULL);
-	pthread_join(thread_id,NULL);
-	printf("\nAFTER Thread\n");
-	exit(0);
+	pthread_t thread1, thread2;
+	int num1 = 1, num2 = 2;
+	pthread_create(&thread1, NULL, print_numbers, &num1);
+	pthread_create(&thread2, NULL, print_numbers, &num2);
+	pthread_join(thread1, NULL);
+	pthread_join(thread2, NULL);
+	
+	return 0;
 }
+
