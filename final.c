c4b4f8d1ce475bd93872e6b9f09f2160ba72a116
mustafa omidian <mustafaomidian@gmail.com>
2023-08-27 13:49:04 +0330

    new

--- final.c
+++ final.c
@@ -1,24 +1,20 @@
 #include <stdio.h>
 #include <pthread.h>
 
-void *print_numbers(void *arg)
+void* Printer(void* num)
 {
-	int *num = (int *)arg;
-	for(int i = 0; i < 5; i++ )
-	{
-		printf("Thread %d: %d\n", *num,i);
-	}
-	return NULL;
-}
-int main()
-{
-	pthread_t thread1, thread2;
-	int num1 = 1, num2 = 2;
-	pthread_create(&thread1, NULL, print_numbers, &num1);
-	pthread_create(&thread2, NULL, print_numbers, &num2);
-	pthread_join(thread1, NULL);
-	pthread_join(thread2, NULL);
-	
-	return 0;
+	int n = *(int*)num;
+	printf("\nthis is %d \n",n);
 }
 
+int main()
+{
+	int num;
+	pthread_t thread[12];
+	for (int i = 0; i < 12; i++)
+	{
+		pthread_create(&thread[i],NULL,Printer,&i);
+		pthread_join(thread[i],NULL);
+	}
+	printf("\nthis is my end of Program\n");
+}
